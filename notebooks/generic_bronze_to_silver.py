6d2f7f6a69da186bed70cd90f7e16cedcdf95cfa
sanket-1997 <sanket-1997>
2025-08-24 10:46:53 +0000

    add table checker

--- notebooks/generic_bronze_to_silver.py
+++ notebooks/generic_bronze_to_silver.py
@@ -1,2 +1,9 @@
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from libs import azureauth as au
+from libs import schema_handler as sh
+
+
+env = au.get_env(spark)
+bronze_table = dbutils.widgets.get("bronze_table")
+print(bronze_table)
