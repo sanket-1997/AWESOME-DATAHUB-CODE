80a4fb5dbed8e8a0cc88c14678c54445c2c2d442
sanket-1997 <sanket-1997>
2025-08-24 18:56:39 +0000

    just adding stuffs

--- notebooks/generic_bronze_to_silver.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from libs import azureauth as au
-from libs import schema_handler as sh
-
-
-env = au.get_env(spark)
-bronze_table = dbutils.widgets.get("bronze_table")
-print(bronze_table)
--- notebooks/load_in_silver.py
+++ notebooks/load_in_silver.py
@@ -1,6 +1,16 @@
 # Databricks notebook source
 from libs import schema_handler
 from libs import datafunctions
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from libs import azureauth as au
+from libs import schema_handler as sh
+
+# COMMAND ----------
+
+dfRaw = spark.read.table("datahub_dev_bronze.consumer.orders")
+display(dfRaw)
+dfRaw.withColumn("quantity", lit(1)).write.mode("append").saveAsTable("datahub_dev_silver.consumer.dim_customers")
 
 # COMMAND ----------
 
